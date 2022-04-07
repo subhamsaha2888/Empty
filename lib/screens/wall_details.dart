3687e9b963317f64ae28bebe9209296d9c861401
subhamsaha2888 <34985892+subhamsaha2888@users.noreply.github.com>
2022-04-07 15:37:20 +0530

    Third

--- lib/screens/wall_details.dart
+++ lib/screens/wall_details.dart
@@ -6,6 +6,8 @@ class WallDetails extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+    );
   }
 }
